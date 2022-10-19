5139977bee554bf77083cb340249ece3d37bc9e2
afonso <afonso.dgc@gmail.com>
2022-10-19 16:03:54 +0100

    atualizar

--- 2_fase/main.c
+++ 2_fase/main.c
@@ -15,9 +15,9 @@ int main(int argc, char **argv)
 
     dict_head = dict_init(argv[1]);
 
-    //TO-DO: aloc grafo
+    //----TO-DO----: aloc/init grafo
 
-    //TO-DO:   solve problem
+    //----TO-DO----:   solve problem
 
 
 
