c7a102535f5ff1d69dc52e4c539bf5f72a946065
afonso <afonso.dgc@gmail.com>
2022-10-24 19:05:17 +0100

    {erros} dijkstra com fila de prioridades

--- WordMutations/dijkstra.c
+++ WordMutations/dijkstra.c
@@ -3,220 +3,255 @@
 #include "graph.h"
 #include <math.h>
 
-#define P (wt[v] + t->cost)
+#define P (wt[v] + t->cost) // soma da distancia de um vertice com o custo de uma aresta 
 
-void GRAPHpfs(Graph *G, int s, int *st, float *wt, int numOfMutations)
+/*********************************************************************
+ * GRAPHpfs()
+ * 
+ * g : graph
+ * origem : index do vertice de origem
+ * destino : index do vertice de destino 
+ * wt : custos
+ * st : antecessores
+ * numOfMutations : (limite do algoritmo) numero de mutações
+ * 
+ * side efects: encontra o caminho mais entre dois vertices
+***********************************************************************/
+void dijkstra(Graph *G, int origem, int destino, int *st, float *wt, int numOfMutations)
 {
-    int PQsize;
-    int *pq, *qp;
-    int custo_max = numOfMutations * numOfMutations;
+    Queu Queu; // fila
+    int custo_max = numOfMutations * numOfMutations; // distancia máxiam atá áà origem
+    int v, w; // variaveis auxiliares
+    node *t; // node auxiliar
 
-    pq = (int *)calloc(G->numOfVertices + 1, sizeof(int));
-    qp = (int *)calloc(G->numOfVertices + 1, sizeof(int));
+    //inicialização da fila
+    PQinit(&Queu, G->numOfVertices);
 
-    int v, w;
-    node *t;
-    PQinit(&PQsize);
+    // inicializa o vetor de antecessores e as distancias a infinito
     for (v = 0; v < G->numOfVertices; v++)
     {
         st[v] = -1;
         wt[v] = HUGE_VAL;
-        PQinsert(v, pq, qp, &PQsize, wt);
     }
 
-    wt[s] = 0.0;
-    PQchange(s, PQsize, pq, qp, wt);
-    while (!PQempty(PQsize))
-        if (wt[v = PQdelmin(&PQsize, pq, qp, wt)] != HUGE_VAL)
-            for (t = G->adjList[v]; t != NULL; t = t->next)
-                if (wt[w = t->vertex] > P && t->cost <= custo_max)
+    wt[origem] = 0.0; // a distancia da origem a ela propria é 0
+    PQinsert(&Queu, origem, wt);
+
+    /* percorre este ciclo até a fila de prioridades ficar vazia 
+       ou até ser descuberto o caminho mais curto até ao destino*/
+    while (!PQempty(Queu) && Queu.pos[destino] != -2){
+        if (wt[v = PQdelmin(&Queu, wt)] != HUGE_VAL){
+            for (t = G->adjList[v]; t != NULL; t = t->next){
+                /* a distancia tem de ser menor que a distancia já calculada e do que a minima */
+                if (wt[w = t->vertex] > P && t->cost <= custo_max) 
                 {
-                    wt[w] = P;
-                    PQchange(w, PQsize, pq, qp, wt);
+                    wt[w] = P; // atualizar a distancia
+
+                    // se não estiver na fila tem de se adicionar primeiro
+                    if (!PQisInQueu(Queu, w)){
+                        PQinsert(&Queu, w, wt);
+                    }
+
                     st[w] = v;
                 }
+            }
+            Queu.pos[v] = -2; // marca como
+            Queu.PQsize--;
+        } else { break; } // se a distancia do elemento prioritário da lista for infinito sai
+    }
 
-    free(qp);
-    free(pq);
+    PQfree(&Queu);
 }
 
-void exch(int i, int j, int *pq, int *qp)
+/******************************************************
+ * exch()
+ * 
+ * argumets:
+ *          Queu : fila
+ *          index1 : index do primeiro elemnto na fila
+ *          index2 : index do segundo elemto na fila
+ * 
+ * side efects: troca dois elemntos numa lista de prioridades
+*******************************************************/
+void exch(Queu *Queu, int index1, int index2)
 {
-    int t;
+    int buffer;
 
-    t = qp[i];
-    qp[i] = qp[j];
-    qp[j] = t;
+    //alterar a fila de prioridades
+    buffer = Queu->queu[index1];
+    Queu->queu[index1] = Queu->queu[index2];
+    Queu->queu[index1] = buffer;
 
-    pq[qp[i]] = i;
-    pq[qp[j]] = j;
+    // alterar a pos(vetor position)
+    Queu->pos[Queu->queu[index1]] = index2;
+    Queu->pos[Queu->queu[index2]] = index1;
 }
 
-void PQinit(int *PQsize)
+/*****************************************************************
+ * inicializar a queu
+ * argumets:
+ *          Queu : fila
+ *          size : numero de vertices do grafo
+ * 
+ * side efects: aloca memoria para a fila e inicializa os seus valores
+*****************************************************************/
+void PQinit(Queu *Queu, int size)
 {
-    *PQsize = 0;
+    Queu->queu = (int*)malloc(sizeof(int)*size);
+    Queu->pos = (int*)malloc(sizeof(int)*size);
+
+    for (int i = 0; i < size; i++){
+        Queu->queu[i] = i;
+        Queu->pos[i] = -1;
+    }
+
+    Queu->PQsize = 0;
 
     return;
 }
-
-void PQinsert(int k, int *pq, int *qp, int *PQsize, float *wt)
-{
-    qp[k] = ++(*PQsize);
-    pq[*PQsize] = k;
-    fixUp(pq, k, qp, wt);
+/***********************************************************
+ * PQfree()
+ * 
+ * side efects: liberta a memoria usada para a fila
+****************************************************************/
+void PQfree(Queu *queu){
+    free(queu->pos);
+    free(queu->queu);
 }
 
-void fixDown(int *pq, int k, int N, int *qp, float *wt)
+/**************************************************************
+ * PQisInQueu()
+ * 
+ * return: retorna 1 se estiver na fila
+ *         retorna 0 se não estiver na fila
+***************************************************************/
+int PQisInQueu(Queu Queu, int index)
 {
-    int j;
-    while (2 * k <= N - 1)
+    // se o pos[index] == -1 ou -2 quer dizer que não esstá na fila
+    if (Queu.pos[index] == -1 || Queu.pos[index] == -2) return 0;
+
+    return 1;
+}
+
+/****************************************************************************************
+ * FixUp()
+ * 
+ * arguments:
+ *          Queu : fila
+ *          indiceChange : indice do vertice que pode ter mudado de prioridade
+ *          wt : vetor de distancias 
+ * 
+ * side efects: ajusta a prioridade de um elemento quando a sua prioridade diminui
+****************************************************************************************/
+void FixUp(Queu *Queu, int indiceChange, float *wt)
+{
+    while (indiceChange > 0 && less(*Queu, (indiceChange - 1)/2 , indiceChange, wt)) {
+        exch(Queu, indiceChange, (indiceChange - 1)/2);
+        indiceChange = (indiceChange - 1)/2;
+    }
+
+
+}
+
+
+/***************************************************************************************************
+ * fixDown()
+ * 
+ * arguments:
+ *          Queu : fila
+ *          indiceChange : indice do vertice que pode ter mudado de prioridade
+ *          wt : vetor de distancias 
+ * 
+ * side efects: ajusta a prioridade de um elemento quando a sua prioridade aumenta
+*****************************************************************************************************/
+void fixDown(Queu *Queu, int indiceChange, float *wt)
+{
+    int child; // indice do nó a descender 
+    int indexNaQueu = Queu->pos[indiceChange]; //valor do index que está a ser movido na queu
+
+    while (2 * indexNaQueu < Queu->PQsize - 1) //irá fazer este ciclo até chegar ás folhas
     {
-        j = 2 * k;
-        if (j < (N - 1) && less(pq[j], pq[j + 1], wt))
-            j++;
-        if (!less(pq[k], pq[j], wt))
+        child = 2 * indexNaQueu + 1; // index do seu primeiro filho na lista de prioridades
+
+        if (child < (Queu->PQsize - 1)) // verifica se o elemento de index "child" não é o unico filho 
+            if (less(*Queu, child, child + 1, wt)) // se a prioridade do child for menor que o child + 1 vai comparar o child +1 com o pai
+                child++;
+        if (!less(*Queu,indexNaQueu, child, wt)) // compara a prioridade do pai com o filho de maior prioridade
             break;
-        exch(pq[k], pq[j], pq, qp);
-        k = j;
+
+        exch(Queu, indexNaQueu, child); //troca os elementos se chegar aqui
+        indexNaQueu = child; //muda o index para o index do filho para ser verificdo na proxima iteração
     }
 }
 
-// tem de comparar com os weights
-int less(int i, int j, float *wt)
+
+
+/***************************************************************************************
+ * less
+ *
+ * arguments:
+ *           Queu : fila
+ *           index1 : index do primeiro elemento na fila
+ *           index2 : index do segundo elemento na fila
+ *           wt : vetor com as distancias á origem
+ * 
+ * side efects: retorna 1 se a distancia á origem do index1 for menor que o index2
+*************************************************************************************/
+int less(Queu Queu, int index1, int index2, float *wt)
 {
-    if (wt[i] < wt[j])
+    if (wt[Queu.queu[index1]] < wt[Queu.queu[index2]])
         return 1;
     return 0;
 }
 
-void PQchange(int k, int PQsize, int *pq, int *qp, float *wt)
+/***************************************************************
+ * PQempty()
+ * 
+ * argumets:    queu : lista 
+ * 
+ * return: numero de elementos na fila
+ *         se o a lista empty retorna 0
+************************************************************/
+int PQempty(Queu queu)
 {
-    fixUp(pq, qp[k], qp, wt);
-    fixDown(pq, qp[k], PQsize, qp, wt);
+    return queu.PQsize;
 }
 
-int PQdelmin(int *PQsize, int *pq, int *qp, float *wt)
+/*****************************************************************
+ * PQinsert()
+ * 
+ * argumets:
+ *          Queu: fila
+ *          index: index vertice que vai ser indserido
+ *          wt: vetor das distancias à origem
+ * 
+ * side efects: insere um elemento na fila
+******************************************************************/
+void PQinsert(Queu *Queu, int index, float *wt)
 {
-    exch(pq[0], qp[*PQsize - 1], pq, qp);
-    fixDown(pq, 0, *PQsize - 1, qp, wt);
-    return pq[--(*PQsize)];
+    Queu->pos[index] = Queu->PQsize;
+    Queu->PQsize++;
+    FixUp(Queu, index, wt);
 }
 
-/************************************************************************************************************************
- *
- * dijkstra() - função que realiza o algoritmo dijkstra
- * arguments:
- *          int origem:     index do vertice de origem do problema
- *          int destino:    index do vertice de destino do problema
- *          graph *grafo:   grafo com:(numOfVertices, numOfEdges, wordSize, numOfMutations, **adjList)
- *          struct caminho_mais_curto resultado: contem o custo do caminho e um vetor de antecessores
- *          int numOfmutations:   numero de caracteres que são permitidos trocar
- *
- * return: uma estrotura com o custo e um vetor com os antecessore de cada vertice
- *         se nao existir caminho o custo será -1
- * side efects: encontra o caminho mais curto entre dois vertices(neste caso entre duas palavras)
- *              e o seu respetivo custo
- *
- *************************************************************************************************************************/
-// void dijkstra(int origem, int destino, Graph *grafo, Caminho *resultado, int numOfmutations)
+/******************************************************************************
+ * PQdelmin()
+ * 
+ * argumets:
+ *          queu: fila
+ *          wt: vetor com as distancias à origem de todos os elementos
+ * 
+ * retorna o index do elemento com mais prioridade da fila de prioridades 
+*********************************************************************************/
+int PQdelmin(Queu *queu, float *wt)
+{
+    int result = queu->queu[0]; // restirar o resultado
+    
+    exch(queu, 0, queu->PQsize - 1);
+    fixDown(queu, queu->PQsize - 1, wt);
 
-// {
-//     int i = 0, v = 0;                                /* variáveis auxiliares */
-//     short int z[grafo->numOfVertices];               /* vértices para os quais se conhece o caminho mínimo */
-//     float dist[grafo->numOfVertices];                /* vetor com os custos dos caminhos */
-//     double min;                                      /* variável auxiliar */
-//     node *no_aux;                                    /* no auxiliar */
-//     int custo_max = numOfmutations * numOfmutations; /* custo máximo que cada aresta pode ter */
+    queu->pos[result] = -2; // mete o pos a -2 para indicar que já se sabe o caminho mais curto
 
-//     resultado->ant = (int *)malloc(sizeof(int) * grafo->numOfVertices); /* vetor dos antecessores */
-
-//     /* Inicializacoes */
-//     for (int i = 0; i < grafo->numOfVertices; i++)
-//     {
-//         /* distancias inicalmente a infinito */
-//         dist[i] = HUGE_VAL;
-//         /* desconhecido o caminho minimo (z[vertice]=0) */
-//         z[i] = 0;
-//         /* inicialmente ningem tem antecessor */
-//         resultado->ant[i] = -1;
-//     }
-
-//     /* carrega os custos dos vertices que estao ligados a origem*/
-//     no_aux = grafo->adjList[origem];
-//     while (no_aux != NULL)
-//     {
-//         // verifiacar se a aresta tem o custo permitido
-//         if (no_aux->cost > custo_max)
-//         {
-//             no_aux = no_aux->next;
-//             continue;
-//         }
-
-//         dist[no_aux->vertex] = no_aux->cost;
-
-//         /* todos os que estiverem nesta lista de adjacencias tem a origem como antecessor */
-//         resultado->ant[no_aux->vertex] = origem;
-
-//         no_aux = no_aux->next;
-//     }
-
-//     z[origem] = 1;    // inicialmente só se conhece o caminho minimo para a origem
-//     dist[origem] = 0; // a distancia da origem a ela mesma é zero
-
-//     /* Laço principal */
-
-//     do
-//     {
-
-//         /* ja sabemos as distancias ate aos vizinhos de z */
-//         /* Encontrando o vertice que deve entrar em z */
-//         min = HUGE_VAL;
-//         for (i = 0; i < grafo->numOfVertices; i++)
-//         {
-//             /* se o vertice ainda nao esta blockeado entra no if*/
-//             if (!z[i])
-//             {
-//                 if (dist[i] > 0 && dist[i] < min)
-//                 {
-//                     min = dist[i];
-//                     v = i;
-//                 }
-//             }
-//         }
-
-//         /* Calculando as distâncias dos novos vizinhos de z */
-//         if (min != HUGE_VAL && v != destino)
-//         {
-//             z[v] = 1; // bloqueia o vertice v
-//             no_aux = grafo->adjList[v];
-//             while (no_aux != NULL)
-//             {
-//                 /* se o vertice ainda nao esta blockeado entra no if*/
-//                 if (!z[no_aux->vertex])
-//                 {
-//                     /*compara a distancia do ate ao nó blockeado mais
-//                     a distancia do nó ate ao outro no de adjacencia com a distancia guardada*/
-//                     /* verifiacar se a aresta tem o custo permitido */
-//                     if (dist[v] + no_aux->cost < dist[no_aux->vertex] && no_aux->cost <= custo_max)
-//                     {
-//                         dist[no_aux->vertex] = dist[v] + no_aux->cost;
-//                         resultado->ant[no_aux->vertex] = v;
-//                     }
-//                 }
-
-//                 no_aux = no_aux->next;
-//             }
-//         }
-//     } while (v != destino && min != HUGE_VAL);
-
-//     // se não existir caminho min é inifinito
-//     // para facilitar retornamos apenas -1
-//     if (min == HUGE_VAL)
-//     {
-//         min = -1;
-//     }
-//     resultado->custo = min;
-
-//     /*retorna uma estrutura que foi passada por argumentos*/
-//     return;
-// }
+    queu->PQsize--;
+    return result;
+}
